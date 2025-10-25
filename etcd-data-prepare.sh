375cde6f46c14546d8f5d94d5ca2bba1079f1fe6
German Zhyvotnikov <gzh@viinex.com>
2025-10-25 22:45:01 +0300

    hassle with `etcd-data`

---
+++ etcd-data-prepare.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+if test -d "etcd-data" ; then
+    echo "etcd-data is already present. remove it first"
+    exit 0
+fi
+
+if test -z `which etcdctl` ; then
+    echo "etcdctl is needed to run this script."
+    echo "please run 'apt install -y etcd-client'"
+    exit 1
+fi
+
+etcdctl snapshot restore etcd-snapshot --data-dir=etcd-data
+find etcd-data/ -type d -exec chmod 777 '{}' ';'
+find etcd-data/ -type f -exec chmod 666 '{}' ';'
